66a42d1d3be9ea44ba17d2a96938ef38e6753abe
Kangning <liu.kangning@163.com>
2015-12-19 16:07:18 +0800

    video funciton

--- js/main.js
+++ js/main.js
@@ -1,4 +1,38 @@
 (function(){
+	var video = {
+		init: function(){
+
+		},
+		bind: function(target, img, callback){
+			this.play(target)
+			this.end(target, img, callback)
+		},
+		play: function(target){
+			target[0].play()
+		},
+		/***
+		* target: (obj) 视频对象
+		* img: (boolen) 默认显示图片，true不显示
+		* callback: (function) 回调函数
+		**/
+		end: function(target, img, callback){
+			target[0].addEventListener('ended', function(){
+				if(!img){
+					var img = target.find('img')
+					target
+						.after(img)
+						.remove()
+				}
+
+				if(callback)callback()
+			},false)
+		}
+	} 
+
+	$(function(){
+		var video_0 = $('#test')
+		video.bind(video_0)
+	})
 })();
 
 $(function() {
