75447e31b0dd94a24928ec15245e17ddabbb42ad
Howe <hhhaohao@163.com>
2015-12-19 16:12:40 +0800

    fullpage

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 (function(){
+
 })();
 
 $(function() {
     FastClick.attach(document.body);
+    $('.main').onePageScroll({
+	    sectionContainer: "section",
+	    easing: "ease",
+	    animationTime: 1000,
+	    pagination: true,
+	    updateURL: false,
+	    beforeMove: function(index) {},
+	    afterMove: function(index) {},
+	    loop: false,
+	    keyboard: true,
+	    responsiveFallback: false 
+	});
 })
