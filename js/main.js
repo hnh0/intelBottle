79242b16712daf6d6dc0f0f3697938f65c48857b
Kangning <liu.kangning@163.com>
2015-12-19 20:33:58 +0800

    rewrite swipe up function

--- js/main.js
+++ js/main.js
@@ -35,6 +35,8 @@
 	})
 })();
 
+
+//手机震动
 window.onload = function() {
 
     //create a new instance of shake.js.
@@ -56,18 +58,59 @@ window.onload = function() {
     }
 };
 
+//判断横竖屏
+window.addEventListener('orientationchange', function(event){
+    if ( window.orientation == 180 || window.orientation==0 ) {
+    	//竖屏
+        console.log("竖屏");
+    }
+    if( window.orientation == 90 || window.orientation == -90 ) {
+    	//横屏
+        console.log("横屏");
+    }
+});
+
+//向上滑动方法
+var swipe = {
+	scrollWrap: '.main',
+	iHeight: $('body').height(),
+	scrolling: false,
+	scrollCurrent: 0,
+	scrollTarget: 1,
+	fSwipe: function(i){
+		var self = this,
+			oThis = $(self.scrollWrap),
+			oSons = oThis.find('section')
+		self.scrollTarget = i
+
+		self.scrolling = !self.scrolling
+
+		oSons.eq(0).after(oThis.find('section.page'+(self.scrollTarget+1)));
+		oThis
+			.animate({
+				marginTop: -self.iHeight
+				}, 600, function() {
+				oSons.eq(0).appendTo(oThis)
+				oThis
+					.css({marginTop: 0})
+
+				self.scrollCurrent = self.scrollTarget
+				self.scrollTarget ++
+
+				self.scrolling = !self.scrolling
+			});
+	}
+}
+
 $(function() {
     FastClick.attach(document.body);
-    $('.main').onePageScroll({
-	    sectionContainer: "section",
-	    easing: "ease",
-	    animationTime: 1000,
-	    pagination: true,
-	    updateURL: false,
-	    beforeMove: function(index) {},
-	    afterMove: function(index) {},
-	    loop: false,
-	    keyboard: true,
-	    responsiveFallback: false 
-	});
+
+	$(swipe.scrollWrap).swipeUp(function(){
+		if(swipe.scrolling){
+			return false;
+		}
+
+		swipe.fSwipe(swipe.scrollTarget)
+	})
+
 })
